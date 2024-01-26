d641ed7d02757840b726bc59333646ece95a3a27
Prabhu Patil <3401727+prabhupatil@users.noreply.github.com>
2024-01-26 19:26:54 +0530

    CORS requestMatchers change generate-otp-by-user-name-and-password

--- src/main/java/com/sericulture/authentication/config/SecurityConfig.java
+++ src/main/java/com/sericulture/authentication/config/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth ->
                         auth
                                 .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
-                                .requestMatchers("/v3/api-docs/**","/auth/register","/auth/login","/auth/refresh-token","/swagger-ui/**","/login","/actuator/**","/v1/userMaster/generate-otp-by-user-name", "/v1/userMaster/verify-otp-by-user-name").permitAll()
+                                .requestMatchers("/v3/api-docs/**","/auth/register","/auth/login","/auth/refresh-token","/swagger-ui/**","/login","/actuator/**","/v1/userMaster/generate-otp-by-user-name-and-password", "/v1/userMaster/verify-otp-by-user-name").permitAll()
                                 .anyRequest().authenticated()
                 );
 
